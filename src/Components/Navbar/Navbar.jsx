adee535daba07ccbbee5f4e1cba08e3fa109f43a
Leonardo Santos <leogsantos5@gmail.com>
2024-05-29 01:25:59 +0100

    Added React E-Portfolio project to Projects component.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -12,8 +12,8 @@ const Navbar = () => {
           <ul className="nav-menu">
             <Link className={activeLink === 'home' ? 'active' : ''} to="home" spy={true} smooth={true} duration={200} offset={-100} onSetActive={() => setActiveLink('home')}>Introduction</Link>
             <Link className={activeLink === 'skills' ? 'active' : ''} to="skills" spy={true} smooth={true} duration={200} offset={-100} onSetActive={() => setActiveLink('skills')}>Skills</Link>
-            <Link className={activeLink === 'experience' ? 'active' : ''} to="experience" spy={true} smooth={true} duration={200} offset={+10} onSetActive={() => setActiveLink('experience')}>Experience</Link>
-            <Link className={activeLink === 'education' ? 'active' : ''} to="education" spy={true} smooth={true} duration={200} offset={-100} onSetActive={() => setActiveLink('education')}>Education</Link>
+            <Link className={activeLink === 'experience' ? 'active' : ''} to="experience" spy={true} smooth={true} duration={200} offset={+0} onSetActive={() => setActiveLink('experience')}>Experience</Link>
+            <Link className={activeLink === 'education' ? 'active' : ''} to="education" spy={true} smooth={true} duration={200} offset={-70} onSetActive={() => setActiveLink('education')}>Education</Link>
             <Link className={activeLink === 'projects' ? 'active' : ''} to="projects" spy={true} smooth={true} duration={200} offset={+10} onSetActive={() => setActiveLink('projects')}>Projects</Link>
 {/*             <Link activeClass="active" to="home" spy={true} smooth={true} duration={200}>Introduction</Link>
             <Link activeClass="active" to="skills" spy={true} smooth={true} offset={-100} duration={200}>Skills</Link>
